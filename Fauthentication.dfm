2bedb4e36cdc22b7b0f65907501c514d95004a0d
slavik <slavikok2@gmail.com>
2016-05-24 14:07:15 +0300

    Ready prototype

--- Fauthentication.dfm
+++ Fauthentication.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Authentication'
-  ClientHeight = 299
-  ClientWidth = 635
+  ClientHeight = 350
+  ClientWidth = 808
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -15,11 +15,24 @@ object Form1: TForm1
   OnDestroy = FormDestroy
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 213
+    Top = 67
+    Width = 49
+    Height = 23
+    Caption = 'Name'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -19
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
   object InitDeviceBtn: TButton
-    Left = 56
-    Top = 152
+    Left = 334
+    Top = 8
     Width = 105
-    Height = 33
+    Height = 48
     Caption = 'Connect Sensor'
     TabOrder = 0
     OnClick = InitDeviceBtnClick
@@ -58,15 +71,44 @@ object Form1: TForm1
     Text = 'SensorSN'
   end
   object ZKFPEngX1: TZKFPEngX
-    Left = 603
-    Top = 267
+    Left = 776
+    Top = 318
     Width = 24
     Height = 24
+    OnFeatureInfo = ZKFPEngX1FeatureInfo
+    OnImageReceived = ZKFPEngX1ImageReceived
+    OnEnroll = ZKFPEngX1Enroll
+    OnCapture = ZKFPEngX1Capture
     ControlData = {
       5450463008545A4B4650456E6700044C656674020003546F7002000557696474
       6802180648656967687402180B456E726F6C6C436F756E7402030B53656E736F
       72496E6465780200095468726573686F6C64020A114F6E65546F4F6E65546872
       6573686F6C64020A10466F7263655365636F6E644D61746368080C4175746F49
       64656E74696679080D4973496D6167654368616E6765080000}
+  end
+  object RegisterBtn: TButton
+    Left = 8
+    Top = 72
+    Width = 105
+    Height = 58
+    Caption = 'Register'
+    Enabled = False
+    TabOrder = 6
+    OnClick = RegisterBtnClick
+  end
+  object firstName: TEdit
+    Left = 183
+    Top = 96
+    Width = 121
+    Height = 21
+    TabOrder = 7
+  end
+  object StatusBar: TListBox
+    Left = 8
+    Top = 136
+    Width = 431
+    Height = 201
+    ItemHeight = 13
+    TabOrder = 8
   end
 end
--- Fauthentication.pas
+++ Fauthentication.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ZKFPEngXControl_TLB, ExtCtrls, OleCtrls;
+  Dialogs, StdCtrls, ZKFPEngXControl_TLB, ExtCtrls, OleCtrls, WideStrings,
+  DBXMySql, DB, SqlExpr;
 
 type
   TForm1 = class(TForm)
@@ -14,12 +15,29 @@ type
     PanelStatus: TPanel;
     SensorIndex: TEdit;
     SensorSN: TEdit;
+    RegisterBtn: TButton;
+    firstName: TEdit;
+    Label1: TLabel;
+    StatusBar: TListBox;
     procedure InitDeviceBtnClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure RegisterBtnClick(Sender: TObject);
+    procedure ZKFPEngX1Enroll(ASender: TObject; ActionResult: WordBool;
+      ATemplate: OleVariant);
+    procedure ZKFPEngX1Capture(ASender: TObject; ActionResult: WordBool;
+      ATemplate: OleVariant);
+    procedure ZKFPEngX1FeatureInfo(ASender: TObject; AQuality: Integer);
+    procedure ZKFPEngX1ImageReceived(ASender: TObject;
+      var AImageValid: WordBool);
+    procedure SaveToFile(str: WideString);
+
   private
     fpcHandle: Integer;
     FFingerName: TStringList;
+    sRegTemplate: WideString;
+    sRegTemplate10: WideString;
+    FID: Integer;
     const EngineVersion: WideString = '10';
 
   public
@@ -34,12 +52,13 @@ implementation
 
 {$R *.dfm}
 
-  procedure TForm1.FormCreate(Sender: TObject);
+procedure TForm1.FormCreate(Sender: TObject);
   begin
     DeviceStatus := False;
     PanelStatus.Color := clRed;
     fpcHandle := ZKFPEngX1.CreateFPCacheDB;
     FFingerName := TStringList.Create;
+    FID:=1;
   end;
 
 
@@ -60,7 +79,134 @@ implementation
        SensorSN.Text := ZKFPEngX1.SensorSN;
        PanelStatus.Color := clLime;
        InitDeviceBtn.Enabled := False;
+       RegisterBtn.Enabled := True;
+       StatusBar.Items.Add('Init device OK!');
     end;
+  end;
+
+  procedure TForm1.RegisterBtnClick(Sender: TObject);
+  begin
+    if (firstName.Text = '')  then begin
+       MessageDlg('Please enter first name ', mtInformation, [mbOK], 0);
+       Exit;
+    end;
+    ZKFPEngX1.CancelEnroll;
+    ZKFPEngX1.EnrollCount := 3;
+    ZKFPEngX1.BeginEnroll;
+    StatusBar.Items.Add('Register start');
+  end;
+
+
+
+procedure TForm1.ZKFPEngX1Capture(ASender: TObject; ActionResult: WordBool; ATemplate: OleVariant);
+var
+  ID, I: Integer;
+  Score, ProcessNum: Integer;
+  RegChange: WordBool;
+  iNew: Integer;
+  sTemp,bTemp: WideString;
+  Temp: WideString;
+  f: TextFile; // файл
+  fName: String[80]; // имя файла
+  pTemplate: OleVariant;
+  bTemplate: OleVariant;
+  strArray  : Array of String;
+
+begin
+    StatusBar.Items.Add('Check Register');
+     ProcessNum := 0;
+     Score := 8;
+     fName:='data.txt';
+     AssignFile(f, fName);
+     Reset(f);
+      
+    While not EOF(f) do // пока не конец файла делать цикл:
+    begin
+        read(f, sTemp);
+        FFingerName.Delimiter:=';;';
+        FFingerName.Text :=  sTemp;
+       //ZKFPEngX1.AddRegTemplateStrToFPCacheDB(fpcHandle, FID, sTemp);
+    end;
+
+    {while FID <100000 do begin
+        Inc(FID);
+       ZKFPEngX1.AddRegTemplateStrToFPCacheDB(fpcHandle, FID, sTemp);
+    end;}
+      ID := ZKFPEngX1.IdentificationInFPCacheDB(fpcHandle, ATemplate, Score, ProcessNum);
+    if ID = -1 then
+        MessageDlg('Помилка скану відпечатка?Score = ' + IntToStr(Score) + ' Processed Number = ' + IntToStr(ProcessNum), mtInformation, [mbOK], 0)
+    else begin
+        MessageDlg('Identify Succeed!', mtInformation, [mbOK], 0);
+    end;
+end;
+
+procedure TForm1.ZKFPEngX1Enroll(ASender: TObject; ActionResult: WordBool; ATemplate: OleVariant);
+  var
+    pTemplate: OleVariant;
+  begin
+    if (not ActionResult) then
+		 MessageDlg('Register failed', mtError, [mbOK], 0)
+	  else begin
+      //After enroll, you can get 9.0&10.0 template at the same time
+      sRegTemplate := ZKFPEngX1.GetTemplateAsStringEx('9');
+      sRegTemplate10 := ZKFPEngX1.GetTemplateAsStringEx('10');
+      if(Length(sRegTemplate) > 0) then begin
+
+        if(Length(sRegTemplate10) > 0) then
+          ZKFPEngX1.AddRegTemplateStrToFPCacheDB(fpcHandle, FID, sRegTemplate10)
+        else
+          MessageDlg('Register 10.0 failed, template length is zero', mtError, [mbOK], 0);
+       // ZKFPEngX1.RemoveRegTemplateFromFPCacheDB(fpcHandle, FID);
+        //ZKFPEngX1.AddRegTemplateToFPCacheDB(fpcHandle, FID, ATemplate);
+        SaveToFile(sRegTemplate10);
+        {pTemplate :=  ZKFPEngX1.DecodeTemplate1(sRegTemplate);
+        ZKFPEngX1.SaveTemplate('fingerprint.tpl', pTemplate);
+        FFingerName.AddObject(firstName.Text, TObject(FID));
+        Inc(FID);}
+        StatusBar.Items.Add('Register success');
+      end else
+       MessageDlg('Register Failed, template length is zero', mtError, [mbOK], 0)
+    end;
+  end;
+
+procedure TForm1.ZKFPEngX1FeatureInfo(ASender: TObject; AQuality: Integer);
+    var
+  sTemp: string;
+begin
+  sTemp := '';
+  if ZKFPEngX1.IsRegister then
+     sTemp := 'Register Status: still press finger' + IntToStr(ZKFPEngX1.EnrollIndex) + 'times!';
+  sTemp := sTemp + ' Fingerprint quality';
+  if AQuality <> 0 then
+     sTemp := sTemp + ' not good, quality=' + IntToStr(ZKFPEngX1.LastQuality)
+  else
+     sTemp := sTemp + ' good, quality=' + IntToStr(ZKFPEngX1.LastQuality);
+  StatusBar.Items.Add(sTemp);
+end;
+
+procedure TForm1.ZKFPEngX1ImageReceived(ASender: TObject; var AImageValid: WordBool);
+  begin
+    with ZKFPEngX1 do
+      PrintImageAt(Self.Canvas.Handle, 500, 10, ImageWidth, ImageHeight);
+  end;
+
+  procedure TForm1.SaveToFile(str:WideString);
+  var
+   f:TextFile;
+   FileDir:String;
+  begin
+   FileDir:='data.txt';
+  AssignFile(f,FileDir);
+  if not FileExists(FileDir) then
+   begin
+    Rewrite(f);
+    CloseFile(f);
+   end;
+  Append(f);
+  str := str + ';;' + firstName.Text;
+  Writeln(f,str);
+  Flush(f);
+  CloseFile(f);
   end;
 
 end.
